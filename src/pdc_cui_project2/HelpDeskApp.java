acbd873ddf48309de78fa97325acac3277b1aa3d
Daniil-AUT <ryj5244@autuni.ac.nz>
2023-10-01 16:09:30 +1300

    Create files
    Create the java classes that would be needed to develop the final AUT helpdesk

---
+++ src/pdc_cui_project2/HelpDeskApp.java
@@ -0,0 +1,16 @@
+package pdc_cui_project2;
+
+/**
+ *
+ * @author Daniil
+ */
+public class HelpDeskApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/pdc_cui_project2/TicketHandler.java
@@ -0,0 +1,9 @@
+package pdc_cui_project2;
+
+/**
+ *
+ * @author Daniil
+ */
+public class TicketHandler {
+    
+}
