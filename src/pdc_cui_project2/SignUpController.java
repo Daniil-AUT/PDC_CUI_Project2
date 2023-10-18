820f129967054987896a8a8f68a4e259da255f0c
Daniil-AUT <ryj5244@autuni.ac.nz>
2023-10-18 15:02:41 +1300

    Addressed code smells in the sign up controller class

--- src/pdc_cui_project2/SignUpController.java
+++ src/pdc_cui_project2/SignUpController.java
@@ -14,22 +14,26 @@ import javax.swing.border.LineBorder;
  * @author Daniil
  */
 public class SignUpController {
+
     private final SignUpView view;
     private final SignUpModel model;
-    
+    private static final int MIN_PASSWORD_LENGTH = 7;
+    private static final Color ERROR_COLOUR = Color.red;
+    private static final Color DEFAULT_COLOUR = Color.black;
+
     public SignUpController(SignUpView view, SignUpModel model) {
         this.view = view;
         this.model = model;
         DataBaseHandler.getDB();
         attachListeners();
     }
-    
+
     private enum UserType {
         ASSISTANT,
         STUDENT,
         CUSTOMER
     }
-    
+
     private enum Field {
         NAME,
         LNAME,
@@ -37,207 +41,213 @@ public class SignUpController {
         PASS,
         CPASS
     }
-    
+
     private void attachListeners() {
         toHomeScreen();
         toAccountScreen();
     }
-    
+
     private void toHomeScreen() {
-        view.backButton.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("Home button has been pressed");
-                    WindowManager.getManager().setHomeVisible(true);
-                    WindowManager.getManager().setSignUpVisible(false);
-                }
-            });
+        view.backButton.addActionListener((ActionEvent e) -> {
+            System.out.println("Home button has been pressed");
+            WindowManager.getManager().setHomeVisible(true);
+            WindowManager.getManager().setSignUpVisible(false);
+        });
     }
-    
+
     private void toAccountScreen() {
-        view.signupButton.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("Signup button has been pressed");
-                    if(isNameValid() == isLNameValid() ==
-                            isEmailValid() == passwordMatch()) {
-                        String name = view.name.getText();
-                        String lName = view.lastName.getText();
-                        String email = view.email.getText();
-                        char[] passChar = view.password.getPassword();
-                        String password = new String(passChar);
-                        if(isNameValid() && isLNameValid() &&
-                            isEmailValid() && passwordMatch()) {
-                            UserType type = UserType.STUDENT;
-                            if (view.assistant.isSelected()) {
-                                model.createUser(name, lName, email, 
-                                        model.types.ASSISTANT, password);
-                                type = UserType.ASSISTANT;
-                            } else if (view.student.isSelected()) {
-                                model.createUser(name, lName, email, 
-                                        model.types.STUDENT, password);
-                                type = UserType.STUDENT;
-                            } else if (view.customer.isSelected()) {
-                                model.createUser(name, lName, email, 
-                                        model.types.CUSTOMER, password);
-                            }
-                            JOptionPane.showMessageDialog(null,"Your Details:\n\n"+
-                                                          model.getDetails());
-                            System.out.println(model.getID());
-                            
-                        }
-                    }
-                }
-            });
+        view.signupButton.addActionListener((ActionEvent e) -> {
+            validateEntry();
+        });
     }
-    
-    
-    private void setError(String errorMessage, Field field) {
-        switch(field) {
+
+    private void validateEntry() {
+        if (checkFields()) {
+            if (validateFields()) {
+                String name = view.name.getText();
+                String lName = view.lastName.getText();
+                String email = view.email.getText();
+                char[] passChar = view.password.getPassword();
+                String password = new String(passChar);
+                getUserType(name, lName, email, password);
+
+                JOptionPane.showMessageDialog(null, "Your Details:\n\n" + model.getDetails());
+                System.out.println(model.getID());
+            }
+        }
+    }
+
+    private void getUserType(String name, String lName, String email, String password) {
+        if (view.assistant.isSelected()) {
+            model.createUser(name, lName, email, model.types.ASSISTANT, password);
+        } else if (view.student.isSelected()) {
+            model.createUser(name, lName, email, model.types.STUDENT, password);
+        } else {
+            model.createUser(name, lName, email, model.types.CUSTOMER, password);
+        }
+    }
+
+    private boolean validateFields() {
+        return isNameValid() && isLNameValid() && isEmailValid() && passwordMatch();
+    }
+
+    private boolean checkFields() {
+        return isNameValid() == isLNameValid() == isEmailValid() == passwordMatch();
+    }
+
+    private void setError(String errorMessage, Field field, boolean error) {
+        switch (field) {
             case NAME:
-                view.nameLabel.setText("Name: " + errorMessage);
-                view.nameLabel.setForeground(Color.red);
-                view.name.setBorder(new LineBorder(Color.red, 1));
+                view.nameLabel.setText(error ? "Name (" + errorMessage + "): "
+                        : "Name: ");
+                view.nameLabel.setForeground(error ? ERROR_COLOUR : DEFAULT_COLOUR);
+                view.name.setBorder(new LineBorder(error ? ERROR_COLOUR : DEFAULT_COLOUR, 1));
                 break;
             case LNAME:
-                view.lName.setText("Last Name: " + errorMessage);
-                view.lName.setForeground(Color.red);
-                view.lastName.setBorder(new LineBorder(Color.red, 1));
+                view.lName.setText(error ? "Last Name (" + errorMessage + "): "
+                        : "Last Name: ");
+                view.lName.setForeground(error ? ERROR_COLOUR : DEFAULT_COLOUR);
+                view.lastName.setBorder(new LineBorder(error ? ERROR_COLOUR : DEFAULT_COLOUR, 1));
                 break;
             case EMAIL:
-                view.emailLabel.setText("Email: " + errorMessage);
-                view.emailLabel.setForeground(Color.red);
-                view.email.setBorder(new LineBorder(Color.red, 1));
+                view.emailLabel.setText(error ? "Email  (" + errorMessage + "): "
+                        : "Email: ");
+                view.emailLabel.setForeground(error ? ERROR_COLOUR : DEFAULT_COLOUR);
+                view.email.setBorder(new LineBorder(error ? ERROR_COLOUR : DEFAULT_COLOUR, 1));
                 break;
             case PASS:
-                view.passLabel.setText("Password: " + errorMessage);
-                view.passLabel.setForeground(Color.red);
-                view.password.setBorder(new LineBorder(Color.red, 1));
+                view.passLabel.setText(error ? "Password (" + errorMessage + "): "
+                        : "Password: ");
+                view.passLabel.setForeground(error ? ERROR_COLOUR : DEFAULT_COLOUR);
+                view.password.setBorder(new LineBorder(error ? ERROR_COLOUR : DEFAULT_COLOUR, 1));
                 break;
             case CPASS:
-                view.cPassLabel.setText("Confirm Password: " + errorMessage);
-                view.cPassLabel.setForeground(Color.red);
-                view.confPassword.setBorder(new LineBorder(Color.red, 1));
-                view.passLabel.setText("Password: " + errorMessage);
-                view.passLabel.setForeground(Color.red);
-                view.password.setBorder(new LineBorder(Color.red, 1));
+                view.cPassLabel.setText(error ? "Confirm Password (" + errorMessage + "): "
+                        : "Confirm Password: ");
+                view.cPassLabel.setForeground(error ? ERROR_COLOUR : DEFAULT_COLOUR);
+                view.confPassword.setBorder(new LineBorder(error ? ERROR_COLOUR : DEFAULT_COLOUR, 1));
+                view.passLabel.setText(error ? "Password (" + errorMessage + "): "
+                        : "Password: ");
+                view.passLabel.setForeground(error ? ERROR_COLOUR : DEFAULT_COLOUR);
+                view.password.setBorder(new LineBorder(error ? ERROR_COLOUR : DEFAULT_COLOUR, 1));
                 break;
         }
     }
 
-    private void setDefault(Field field) {
-        switch(field) {
-            case NAME:
-                view.nameLabel.setText("Name:");
-                view.nameLabel.setForeground(Color.black);
-                view.name.setBorder(new LineBorder(Color.black, 1));
-                break;
-            case LNAME:
-                view.lName.setText("Last Name:");
-                view.lName.setForeground(Color.black);
-                view.lastName.setBorder(new LineBorder(Color.black, 1));
-                break;
-            case EMAIL:
-                view.emailLabel.setText("Email:");
-                view.emailLabel.setForeground(Color.black);
-                view.email.setBorder(new LineBorder(Color.black, 1));
-                break;
-            case PASS:
-                view.passLabel.setText("Password:");
-                view.passLabel.setForeground(Color.black);
-                view.password.setBorder(new LineBorder(Color.black, 1));
-                break;
-            case CPASS:
-                view.passLabel.setText("Password:");
-                view.passLabel.setForeground(Color.black);
-                view.password.setBorder(new LineBorder(Color.black, 1));
-                view.cPassLabel.setText("Confirm Password:");
-                view.cPassLabel.setForeground(Color.black);
-                view.confPassword.setBorder(new LineBorder(Color.black, 1));
-                break;            
-        }
+    private boolean isNameEmpty() {
+        return view.name.getText().isBlank();
     }
-    
+
+    private boolean isNameOnlyLetters() {
+        String regex = "^[a-zA-Z]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(view.name.getText());
+        return matcher.matches();
+    }
+
     private boolean isNameValid() {
-        String name = view.name.getText();
-        String regex = "^[a-zA-Z]+$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(name);
-        
-        if(name.isBlank()) {
-            setError("Must Contain At Least 1 Character", Field.NAME);
+        if (isNameEmpty()) {
+            setError("Must contain at least 1 character", Field.NAME, true);
             return false;
         }
-        if (!matcher.matches()) {
-            setError("Must Only Contain Letters (No Blank Spaces)", Field.NAME);
+        if (!isNameOnlyLetters()) {
+            setError("Must only contain letters (no blank spaces)", Field.NAME, true);
             return false;
         }
-        
-        setDefault(Field.NAME);
+        setError("", Field.NAME, false);
         return true;
     }
-    
+
+    private boolean isLNameEmpty() {
+        return view.lastName.getText().isBlank();
+    }
+
+    private boolean isLNameOnlyLetters() {
+        String regex = "^[a-zA-Z]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(view.lastName.getText());
+        return matcher.matches();
+    }
+
     private boolean isLNameValid() {
-        String lastname = view.lastName.getText();
-        String regex = "^[a-zA-Z]+$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(lastname);
-        
-        if(lastname.isBlank()) {
-            setError("Must Contain At Least 1 Character", Field.LNAME);
+        if (isLNameEmpty()) {
+            setError("Must contain at least 1 character", Field.LNAME, true);
             return false;
         }
-        if (!matcher.matches()) {
-            setError("Must Only Contain Letters (No Blank Spaces)", Field.LNAME);
+        if (!isLNameOnlyLetters()) {
+            setError("Must only contain letters (no blank spaces)", Field.LNAME, true);
             return false;
         }
-        setDefault(Field.LNAME);
+        setError("", Field.LNAME, false);
         return true;
     }
-    private boolean isEmailValid() {
-        String email = view.email.getText();
+
+    private boolean isEmailEmpty() {
+        return view.email.getText().isBlank();
+    }
+
+    private boolean isEmailValidFormat() {
         String regex = "^[a-zA-Z0-9]+@mail\\.com$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
-        
-        if(email.isBlank()) {
-            setError("Cannot Be Left Blank", Field.EMAIL);
+        Matcher matcher = pattern.matcher(view.email.getText());
+        return matcher.matches();
+    }
+
+    private boolean isEmailValid() {
+        if (isEmailEmpty()) {
+            setError("Cannot be left blank", Field.EMAIL, true);
             return false;
         }
-        if(email.contains(" ")) {
-            setError("Cannot Have Blank Spaces",Field.EMAIL);
+        if (view.email.getText().contains(" ")) {
+            setError("Cannot have blank spaces", Field.EMAIL, true);
             return false;
         }
-        if (!matcher.matches()) {
-            setError("Cannot contain special characters "
-                    + "and have to end with '@mail.com'", Field.EMAIL);
+        if (!isEmailValidFormat()) {
+            setError("Cannot contain special characters and have to end with '@mail.com'",
+                    Field.EMAIL, true);
             return false;
         }
-        setDefault(Field.EMAIL);
+        setError("", Field.EMAIL, false);
         return true;
     }
-    
-    private boolean passwordMatch() {
-        char[] password = view.password.getPassword();
-        char[] confPassword = view.confPassword.getPassword();
-        boolean passwordsMatch = Arrays.equals(password, confPassword);
-        if(password.length <= 7) {
-            setError("Must Be Greater Than 7 Characters In Length",
-                    Field.PASS);
-            return false;
-        }
-        for(char c : password) {
-            if(c == ' ') {
-                setError("Blank Spaces Are Not Allowed", Field.PASS);
+
+    private boolean isPasswordEmpty() {
+        return view.password.getPassword().length == 0;
+    }
+
+    private boolean isPasswordLongEnough() {
+        return view.password.getPassword().length >= MIN_PASSWORD_LENGTH;
+    }
+
+    private boolean isPasswordValidFormat() {
+        for (char c : view.password.getPassword()) {
+            if (c == ' ') {
                 return false;
             }
         }
-        if (!passwordsMatch) {
-            setError("Passwords Are Not The Same", Field.CPASS);
+        setError("", Field.PASS, false);
+        return true;
+    }
+
+    private boolean passwordMatch() {
+        if (isPasswordEmpty()) {
+            setError("Cannot be left blank", Field.PASS, true);
             return false;
         }
-        
-        setDefault(Field.CPASS);
+        if (!isPasswordLongEnough()) {
+            setError("Must be greater than " + MIN_PASSWORD_LENGTH
+                    + " characters in length", Field.PASS, true);
+            return false;
+        }
+        if (!isPasswordValidFormat()) {
+            setError("Blank spaces are not allowed", Field.PASS, true);
+            return false;
+        }
+        if (!Arrays.equals(view.password.getPassword(), view.confPassword.getPassword())) {
+            setError("Passwords are not the same", Field.CPASS, true);
+            return false;
+        }
+        setError("", Field.CPASS, false);
         return true;
     }
 }
